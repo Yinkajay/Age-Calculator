9a4c0efc3181149add13f86f082b003e0a75f0bf
Adeyinka Junaid <yinkajunn@gmail.com>
2023-05-20 01:43:30 +0100

    Layout done

--- src/components/AgeDisplay.jsx
+++ src/components/AgeDisplay.jsx
@@ -7,49 +7,65 @@ const AgeDisplay = () => {
     const [days, setDays] = useState('--')
 
     const calcBirthday = (birthdate) => {
-        console.log(birthdate)
         let { day, month, year } = birthdate
-        // month = month - 1
-        const birthday = new Date(`${year},${month},${day}`)
-        console.log(birthday)
-        const birthdayFrom1970 = birthday.getTime()
-        console.log(birthdayFrom1970)
+
+        const birthDate = new Date(`${year},${month},${day}`)
+        console.log(birthDate)
 
         const today = new Date()
         console.log(today)
-        const todayFrom1970 = today.getTime()
-        console.log(todayFrom1970)
-        const timeSinceBirthday = todayFrom1970 - birthdayFrom1970
-        console.log(timeSinceBirthday)
 
-        const yearsSinceBirthday = Math.floor(timeSinceBirthday / 31556952000)
-        console.log(yearsSinceBirthday)
-        setYears(yearsSinceBirthday)
+        const timeDiff = today.getTime() - birthDate.getTime();
 
-        const remainingMonths = timeSinceBirthday % 31556952000
-        console.log(remainingMonths)
-        const monthsSinceBirthday = (remainingMonths / 2629746000)
-        console.log(Math.floor(monthsSinceBirthday))
-        setMonths(Math.floor(monthsSinceBirthday))
+        const millisecondsPerDay = 24 * 60 * 60 * 1000;
+        const millisecondsPerMonth = 30.44 * millisecondsPerDay;
+        const millisecondsPerYear = 365.25 * millisecondsPerDay;
 
-        const remainingDays = remainingMonths % 2629746000
-        console.log(remainingDays)
-        const daysSinceBirthday = remainingDays / 86400000
-        console.log(daysSinceBirthday)
-        setDays(Math.ceil(daysSinceBirthday))
 
+        setYears(Math.floor(timeDiff / millisecondsPerYear))
+        setMonths(Math.floor((timeDiff % millisecondsPerYear) / millisecondsPerMonth));
+        setDays(Math.round(((timeDiff % millisecondsPerYear) % millisecondsPerMonth) / millisecondsPerDay));
 
     }
     return (
-        <div>
+        <div className="display-card">
             <BirthdayForm onSubmitBirthdate={calcBirthday} />
             <section>
-                <h1>{years} years</h1>
-                <h1>{months} months</h1>
-                <h1>{days} days</h1>
+                <h1> <span className="date-field">{years}</span> years</h1>
+                <h1><span className="date-field">{months}</span> months</h1>
+                <h1><span className="date-field">{days}</span> days</h1>
             </section>
         </div>
     )
 }
 
 export default AgeDisplay
+
+
+// const birthday = new Date(`${year},${month},${day}`)
+// console.log(birthday)
+// const birthdayFrom1970 = birthday.getTime()
+// console.log(birthdayFrom1970)
+
+// const today = new Date()
+// console.log(today)
+// const todayFrom1970 = today.getTime()
+// console.log(todayFrom1970)
+// const timeSinceBirthday = todayFrom1970 - birthdayFrom1970
+// console.log(timeSinceBirthday)
+
+// const yearsSinceBirthday = Math.floor(timeSinceBirthday / 31556952000)
+// console.log(yearsSinceBirthday)
+// setYears(yearsSinceBirthday)
+
+// const remainingMonths = timeSinceBirthday % 31556952000
+// console.log(remainingMonths)
+// const monthsSinceBirthday = (remainingMonths / 2629746000)
+// console.log(Math.floor(monthsSinceBirthday))
+// setMonths(Math.floor(monthsSinceBirthday))
+
+// const remainingDays = remainingMonths % 2629746000
+// console.log(remainingDays)
+// const daysSinceBirthday = remainingDays / 86400000
+// console.log(daysSinceBirthday)
+// setDays(Math.ceil(daysSinceBirthday))
--- src/components/BirthdayForm.jsx
+++ src/components/BirthdayForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import buttonImage from '../../assets/images/icon-arrow.svg'
 
 const BirthdayForm = (props) => {
     const [birthday, setBirthday] = useState({
@@ -6,44 +7,91 @@ const BirthdayForm = (props) => {
         month: '',
         year: ''
     })
+    const [validDay, setValidDay] = useState(true)
+    const [validMonth, setValidMonth] = useState(true)
+    const [validYear, setValidYear] = useState(true)
+
+
+    function getInvalidField(year, month, day) {
+        const date = new Date(year, month - 1, day);
+
+        // Check year
+        if (date.getFullYear() !== year) {
+            return 'year';
+        }
+
+        // Check month
+        if (date.getMonth() !== month - 1) {
+            return 'month';
+        }
+
+        // Check day
+        if (date.getDate() !== day) {
+            return 'day';
+        }
+
+        // Date is valid
+        return null;
+    }
 
     const formSubmitHandler = (e) => {
         e.preventDefault()
-        if (!birthday.day || !birthday.month || !birthday.year) return
-            console.log(birthday)
+
+        if (getInvalidField(birthday.year, birthday.month, birthday.day) === 'year') {
+            setValidYear(false)
+        } else if (getInvalidField(birthday.year, birthday.month, birthday.day) === 'month') {
+            setValidMonth(false)
+        } else if (getInvalidField(birthday.year, birthday.month, birthday.day) === 'day') {
+            setValidDay(false)
+        }
+
+        if (!birthday.day || !birthday.month || !birthday.year) {
+            setValidDay(false)
+            setValidMonth(false)
+            setValidYear(false)
+            return
+        }
+        console.log(birthday)
+
         props.onSubmitBirthdate(birthday)
     }
 
     return (
         <section>
-            <form onSubmit={formSubmitHandler}>
-                <div>
-                    <div>
+            <form onSubmit={formSubmitHandler} className="birthday-form">
+                <div className="form-inputs">
+                    <div className="form-input">
                         <label htmlFor="">
                             DAY
                         </label>
                         <input type="number" value={birthday.day} onChange={(e) => setBirthday({
                             ...birthday, day: +e.target.value
-                        })} />
+                        })} placeholder="DD" />
+                        {!validDay && <p>Must be a valid day</p>}
                     </div>
-                    <div>
+                    <div className="form-input">
                         <label htmlFor="">
                             MONTH
                         </label>
                         <input type="number" value={birthday.month} onChange={(e) => setBirthday({
                             ...birthday, month: +e.target.value
-                        })} />
+                        })} placeholder="MM" />
+                        {!validMonth && <p>Must be a valid month</p>}
                     </div>
-                    <div>
+                    <div className="form-input">
                         <label htmlFor="">
                             YEAR
                         </label>
                         <input type="number" value={birthday.year} onChange={(e) => setBirthday({
                             ...birthday, year: +e.target.value
-                        })} />
+                        })} placeholder="YYYY" />
+                        {!validYear && <p>Must be in the past</p>}
                     </div>
                 </div>
-                <button>Calculate</button>
+                <button type="submit">
+                    <img src={buttonImage} alt="calculate-button" />
+                </button>
+                <hr />
             </form>
         </section >
     )
